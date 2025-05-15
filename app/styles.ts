79c92f8346175c77c7561d4b0c7eac2c60eefe5a
eldief <47423029+eldief@users.noreply.github.com>
2025-05-15 13:57:17 +0200

    fix layout height on dynamic mobile

--- app/styles.ts
+++ app/styles.ts
@@ -3,7 +3,7 @@ import { css } from '@/styled-system/css'
 export const layoutWrapper = css({
   position: 'relative',
   height: '100%',
-  minHeight: '100vh',
+  minHeight: '100dvh',
   width: '100%',
   display: 'flex',
   flexDirection: 'column',
