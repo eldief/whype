2d8b1f3d1ff6364d8fd1e44fc7e38c7ca7dcf648
eldief <47423029+eldief@users.noreply.github.com>
2025-10-09 09:51:33 +0200

    chore: remove unused const

--- constants/websocket.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  type WebSocketReadyState =
-    | typeof WebSocket.CONNECTING
-    | typeof WebSocket.OPEN
-    | typeof WebSocket.CLOSING
-    | typeof WebSocket.CLOSED
-}
-
-export {}
