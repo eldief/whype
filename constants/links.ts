59898190ff1f2b2c554bba814a08fbe8b5c452c3
Yadheedhya.i <79125868+Yadheedhya06@users.noreply.github.com>
2025-11-18 13:20:45 +0800

    site url
    :)

--- constants/links.ts
+++ constants/links.ts
@@ -1,5 +1,5 @@
 const LINKS = {
-  SITE_URL: 'https://whype.zyx',
+  SITE_URL: 'https://whype.xyz',
   COMMIT_HASH: `https://github.com/eldief/whype/commit/${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7)}`,
   DOCS: 'https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/hyperevm/wrapped-hype',
   GITHUB: 'https://github.com/eldief/whype',
